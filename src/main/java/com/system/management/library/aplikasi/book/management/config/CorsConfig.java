8c69b84f1f74cea08bbefcad3107d82df0db3376
Dikin19 <muhamadsodikin191919@gmail.com>
2025-10-12 00:05:10 +0700

    chore(Cors):cors permition

--- src/main/java/com/system/management/library/aplikasi/book/management/config/CorsConfig.java
+++ src/main/java/com/system/management/library/aplikasi/book/management/config/CorsConfig.java
@@ -2,24 +2,26 @@ package com.system.management.library.aplikasi.book.management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // semua endpoint
-                        .allowedOrigins("*") // izinkan semua origin
-                        .allowedMethods("*") // izinkan semua metode HTTP
-                        .allowedHeaders("*") // izinkan semua header
-                        .allowCredentials(false); // harus false kalau pakai allowedOrigins("*")
-            }
-        };
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // jika ingin kirim cookie/token
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://library.msodikin.web.id"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setMaxAge(3600L); // cache preflight response
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
-    
