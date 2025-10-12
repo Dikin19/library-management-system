4ff3d6b5b51cde87bd057247567f79138f9c1df9
Dikin19 <muhamadsodikin191919@gmail.com>
2025-10-12 23:59:56 +0700

    chore(Cors):cors permition

--- src/main/java/com/system/management/library/aplikasi/book/management/MainApp.java
+++ src/main/java/com/system/management/library/aplikasi/book/management/MainApp.java
@@ -1,14 +1,35 @@
 package com.system.management.library.aplikasi.book.management;
 
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class MainApp {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(MainApp.class, args);
+//        System.out.println("Server can be run on http://localhost:8080 ");
+//	}
+//
+//}
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Library Management API",
+                version = "1.0",
+                description = "API documentation for Library Management System"
+        )
+)
 public class MainApp {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MainApp.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class, args);
         System.out.println("Server can be run on http://localhost:8080 ");
-	}
-
+    }
 }
+
