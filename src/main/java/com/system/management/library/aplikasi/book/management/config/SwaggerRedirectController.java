1009a76dd8362472a3f28e4d8fd8559a5131aba3
Dikin19 <muhamadsodikin191919@gmail.com>
2025-10-13 01:38:36 +0700

    chore(Cors):cors permition

---
+++ src/main/java/com/system/management/library/aplikasi/book/management/config/SwaggerRedirectController.java
@@ -0,0 +1,13 @@
+package com.system.management.library.aplikasi.book.management.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SwaggerRedirectController {
+
+    @GetMapping("/")
+    public String redirectToSwagger() {
+        return "redirect:/swagger-ui/index.html";
+    }
+}
